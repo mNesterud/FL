48ef8a5a466f0fa3686694296d5f564da5c2c7b9
mNesterud <mnesterud@gmail.com>
2016-10-13 20:31:05 +0200

    första commit

---
+++ FL/FL/index.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FL
+{
+    public partial class index : System.Web.UI.Page
+    {
+        int tryck = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["knappTryck"] != null)
+            {
+                tryck = Convert.ToInt32(Session["knappTryck"]);
+            }
+            if (!IsPostBack)
+            {
+                Label1.Text = "Välkommen";
+                Session["knappTryck"] = 0;
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            tryck++;
+            Session["knappTryck"] = tryck;
+            Label1.Text = "Du har tryckt på knappen " + tryck.ToString() + " gånger!";
+        }
+
+    }
+}
---
+++ FL/FL/sida2.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="sida2.aspx.cs" Inherits="FL.sida2" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="test">då</div>
+</asp:Content>
